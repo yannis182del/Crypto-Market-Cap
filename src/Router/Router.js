0f5411137bca060b3ebed920147e69253682cf5c
Yannis <yannis.bouacida@appliedtechnology.se>
2020-04-13 17:16:23 +0200

    added a route for coin scanner

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { RouterUl, StyledLink, LinkDiv } from "./style";
-import MarketTable from "../Components/Tables/MartketTable"
-import CoinsTables from "../Components/Tables/CoinsTable"
+import MarketTable from "../Components/Tables/MartketTable";
+import CoinsTables from "../Components/Tables/CoinsTable";
 
 export default function Routing() {
   return (
@@ -18,22 +18,22 @@ export default function Routing() {
                 <StyledLink to="/exchanges">Exhanges</StyledLink>
               </li>
               <li>
-                <StyledLink to="/users">Users</StyledLink>
+                <StyledLink to="/trending">Trending</StyledLink>
               </li>
               <li>
-                <StyledLink to="/trending">Trending</StyledLink>
+                <StyledLink to="/scanner">Coin scanner</StyledLink>
               </li>
             </LinkDiv>
           </RouterUl>
         </nav>
-
         <Switch>
           <Route path="/exchanges">
-          <MarketTable />
-          </Route>
-          <Route path="/users">
+            <MarketTable />
           </Route>
           <Route path="/">
+            <CoinsTables />
+          </Route>
+          <Route path="/scanner">
             <CoinsTables />
           </Route>
         </Switch>
